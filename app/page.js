7f538cf8cb1326cfd67c2ac4dea0c30ee2795757
Boss Codes <jadsaderkarate@gmail.com>
2023-10-23 21:22:31 +0300

    CSSLayoutChanges

--- app/page.js
+++ app/page.js
@@ -4,14 +4,18 @@ import MailLink from './mailLink'
 import ConstructionImage from '../public/websiteUnderConstruction.png'
 export default function Home() {
   return (
-    <div>
+    <div className="wholeBody">
+      <div className="title">
       <h1> Personal Website in the works...
         </h1>
-        <Image src={ConstructionImage} width={512} height={512}/>
+        </div>
+        <Image className="constructionImage" src={ConstructionImage} width={512} height={512}/>
+        <div className="contactInfo">
         <h2>
           Feel free to contact me: 
         </h2>
         <MailLink />   
+        </div>
          </div>
   
   )
