3659cf573d230d5a40cf4d3c51cf2d302e14a4bc
Boss Codes <jadsaderkarate@gmail.com>
2023-10-23 20:20:50 +0300

    finalTest

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <h1> Personal Website in the works...
         </h1>
         <h2>
-          Contact me: 
+          Feel free to contact me: 
         </h2>
         <div  className="mailLink" onClick={(e) => {window.location.href ='mailto:info@jadsader.com';}}>
           <h2>
